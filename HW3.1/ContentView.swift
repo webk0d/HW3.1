cc74b4e1876ed9332733088ec5bbec549e085fce
webk0d <webk0d@ya.ru>
2022-01-20 18:47:28 +0300

    Initial Commit

---
+++ HW3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.1
+//
+//  Created by Сергей Чумаков on 20.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.1/HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1App.swift
+//  HW3.1
+//
+//  Created by Сергей Чумаков on 20.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
